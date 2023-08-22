e8e52e7481a6ba24d39c47f88707dd8d82b67ab1
scarlet8714 <scarlet8174@gmail.com>
2023-08-22 10:21:07 +0800

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,51 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+  const [tasklist, setTaskList] = useState([]);
+  const [newtask,  setNewTask] = useState("");
+  const handleValue = (e) => {
+    setNewTask(e.target.value);
+  }
+  const addTask = () => {
+    let taskObj = {
+      id: tasklist.length === 0 ? 1 : tasklist[tasklist.length - 1].id + 1,
+      task: newtask,
+      isCompleted: false
+    }
+    setTaskList([...tasklist, taskObj]);
+  }
+  const deleteTask = (id) =>{
+    setTaskList(tasklist.filter((taskitem) => taskitem.id !== id));
+  }
+  const setCompleted = (id) => {
+    setTaskList(
+      tasklist.map((item) => {
+        if(item.id === id){
+          return {...item, isCompleted: true};
+        } else {
+          return item;
+        }
+      })
+    );
+    
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='m-5'>
+      <h1>TaskList</h1>
+      <input className='form-control' onChange={handleValue} /><br />
+      <button className=' btn btn-warning mb-3' onClick={addTask}>Add</button>
+      {tasklist.map((item) => (
+        <div className= {item.isCompleted ? 'd-flex mb-3 bg-danger': 'd-flex mb-3'}>
+          <h3 className='my-auto col-6'>{item.task}</h3>
+          <button className='btn btn-primary ms-3 col-3' onClick={() => setCompleted(item.id)}>complete</button>
+          <button className='btn btn-primary ms-3 col-1' onClick={() => deleteTask(item.id)}>X</button>
+        </div>
+      ))}
     </div>
   );
+
 }
 
 export default App;
